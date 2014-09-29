2b7052beff20836d48c5f76acf001c4b2f68ebc7
Lisa-MAria Mehta <lisa-maria.mehta@ubicare.com>
2014-09-28 20:09:39 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HartlFirst::Application.config.secret_token = 'a1ee9272d64b9d5297025a803eaa4e9c06054594205eb3738268f85cbbc16cea085f5549373f004144b9300f95f02099ef8e6129096e46b2658a0c8e22c5483d'
